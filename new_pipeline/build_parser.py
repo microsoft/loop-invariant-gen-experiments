164e9021305e18f8f146dc9ac894a00fe9ffbe66
Adharsh Kamath <t-adkamath@microsoft.com>
2023-08-07 08:51:12 +0530

    remov elocal test

--- new_pipeline/build_parser.py
+++ new_pipeline/build_parser.py
@@ -10,15 +10,3 @@ Language.build_library(
     'tree_sitter_lib/vendor/tree-sitter-c'
   ]
 )
-
-
-"""
-Clone the tree-sitter-cpp repository to tree_sitter_lib/vendor/
-The output of this build is tree_sitter_lib/build/cpp-tree-sitter.so
-"""
-Language.build_library(
-    'tree_sitter_lib/build/cpp-tree-sitter.so',
-    [
-        'tree_sitter_lib/vendor/tree-sitter-cpp'
-    ]
-)
