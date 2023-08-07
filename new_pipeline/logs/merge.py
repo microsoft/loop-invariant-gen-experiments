09fa8d412b7ed3e96f06514241d28b6fbc895252
Adharsh Kamath <t-adkamath@microsoft.com>
2023-08-07 09:46:25 +0530

    latest non-termination

--- new_pipeline/logs/merge.py
+++ new_pipeline/logs/merge.py
@@ -2,26 +2,29 @@ import json
 
 
 with open("loopy_2023_08_03_20_54_22/final.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-    logs = data["logs"]
-    new_logs = []
-    for x, i in enumerate(logs):
-        if "inter_procedural" in i.keys() or "multi_loop" in i.keys():
-            if x in data["stats"]["failure"]:
-                data["stats"]["failure"].remove(x)
-            continue
-        else:
-            new_logs.append(i)
-    data["logs"] = new_logs
-    data["stats"]["total"] = len(new_logs)
-    data["stats"]["success_count"] = len(data["stats"]["success"])
-    data["stats"]["failure_count"] = len(data["stats"]["failure"])
-    data["stats"]["success_rate"] = data["stats"]["success_count"] / data["stats"]["total"]
+    with open("loopy_2023_08_07_03_49_07/final.json", "r", encoding="utf-8") as f2:
+        data = json.load(f)
+        data2 = json.load(f2)
+        logs = data["logs"]
+        logs2 = data2["logs"]
+        new_logs = []
+        for x, i in enumerate(logs2):
+            if "inter_procedural" in logs[x].keys() or "multi_loop" in logs[x].keys():
+                if x in data2["stats"]["failure"]:
+                    data2["stats"]["failure"].remove(x)
+                continue
+            else:
+                new_logs.append(i)
+        data2["logs"] = new_logs
+        data2["stats"]["total"] = len(new_logs)
+        data2["stats"]["success_count"] = len(data2["stats"]["success"])
+        data2["stats"]["failure_count"] = len(data2["stats"]["failure"])
+        data2["stats"]["success_rate"] = data2["stats"]["success_count"] / data2["stats"]["total"]
 
-    with open(
-        "loopy_2023_08_03_20_54_22/final_merged.json", "w", encoding="utf-8"
-    ) as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+        with open(
+            "loopy_2023_08_07_03_49_07/final_merged.json", "w", encoding="utf-8"
+        ) as f3:
+            json.dump(data2, f3, indent=4, ensure_ascii=False)
 
 
 # with open("final_rechecked_logs.json", "r", encoding='utf-8') as f:
