336e8b4d76d4329f5478cdc0e336ae19d5c8e3ed
Adharsh Kamath <t-adkamath@microsoft.com>
2023-08-09 10:06:35 +0530

    new mods

--- pipeline/logs/restat.py
+++ pipeline/logs/restat.py
@@ -3,18 +3,19 @@ import json
 with open("without_nudge_merged.json", "r", encoding='utf-8') as f:
     data = json.load(f)
     logs  = data["logs"]
-    stats = {"success": [], "failure": [], "success_count": 0, "failure_count": 0, "total": 0, "success_rate": 0}
+    stats = {"success": [], "failure": [], "skipped": [], "success_count": 0, "failure_count": 0, "total": 0, "success_rate": 0}
     for x, i in enumerate(logs):
         if "error" in i.keys():
-            stats["failure"].append(x)
+            stats["skipped"].append(x)
         elif i["checker_output"] or i["checker_output_after_prune"]:
             stats["success"].append(x)
         else:
             stats["failure"].append(x)
     stats["success_count"] = len(stats["success"])
     stats["failure_count"] = len(stats["failure"])
+    stats["skipped_count"] = len(stats["skipped"])
     stats["total"] = len(logs)
-    stats["success_rate"] = len(stats["success"]) / len(logs)
+    stats["success_rate"] = stats["success_count"] / (stats["success_count"] + stats["failure_count"])
     data["stats"] = stats
     with open("without_nudge_merged_restat.json", "w", encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
