78411ca1e9e6fb7cf7642aaef1148ae0fe4607e7
Adharsh Kamath <t-adkamath@microsoft.com>
2023-08-09 09:21:41 +0530

    local test

--- pipeline/frama_c.py
+++ pipeline/frama_c.py
@@ -1080,29 +1080,3 @@ def parse_log(logfile, cfile):
             )
 
         return failed_checker_input, checker_error_message, analysis
-
-code = """extern void __VERIFIER_error() __attribute__((noreturn));
-void __VERIFIER_assert (int cond) { if (!cond) __VERIFIER_error (); }
-int unknown(){int x; return x;}
-unsigned int __VERIFIER_nondet_uint();
-void errorFn() {ERROR: goto ERROR;}
-# 1 "MAP/UNSAFE-exbench/TRACER-test3-unsafe.tmp.c"
-# 1 "<command-line>"
-# 1 "MAP/UNSAFE-exbench/TRACER-test3-unsafe.tmp.c"
-# 22 "MAP/UNSAFE-exbench/TRACER-test3-unsafe.tmp.c"
-
-void main(){
-  int x=0;
-  int y=0;
-
-  if (unknown())
-    x = 5;
-  else
-    y = 10;
-
-  __VERIFIER_assert(!( x==5 || y==10 ));
-  return;
-}"""
-fb = FramaCBenchmark()
-code = fb.preprocess(code)
-print(code)
