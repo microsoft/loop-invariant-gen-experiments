af8619f25536b473034c857a89b436e91440ad7b
Adharsh Kamath <t-adkamath@microsoft.com>
2023-09-06 01:39:50 +0530

    codellama added

--- src/local_llm.py
+++ src/local_llm.py
@@ -1,5 +1,7 @@
 import json
+import os
 import subprocess
+
 from llm import LLMClient
 
 
@@ -10,10 +12,11 @@ class LLMLocalClient(LLMClient):
     def chat_batch(self, dataset_path: str):
         """This should be for the local LLM client."""
 
-        cmd = f"OMP_NUM_THREADS=4 torchrun --nproc_per_node 4 llama_2.py --inputs {dataset_path}"
-        p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+        env = os.environ.copy()
+        env["OMP_NUM_THREADS"] = 4
+        cmd = f"torchrun --nproc_per_node 4 llama_2.py --inputs {dataset_path}"
+        # p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, env=env)
         # output, err = p.communicate()
         # output = output.decode("utf-8")
 
-        print(p)
-
+        print(cmd)
